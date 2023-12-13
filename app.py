2be3272481d719269bc9921358ccfa17017c2b5b
Rodemallgames <136923575+Rodemallgames@users.noreply.github.com>
2023-12-12 21:16:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():'
-    return 'Hello and this is the official Rodemallgames Development website. We are a small Roblox development group that made a game with over 500k visits. Thanks for visiting!'
+    return 'Thanks for visiting!'
 
 
 if __name__ == "__main__":
