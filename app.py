811f9750d15d961d004d48e1ddd3cfd01dcd42cf
Rodemallgames <136923575+Rodemallgames@users.noreply.github.com>
2023-12-12 20:22:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'test'
+def hello_world():'
+    return 'Hello and this is the official Rodemallgames Development website. We are a small Roblox development group that made a game with over 500k visits. We will continue development on the website but, for now, it will look like this. Thanks for visiting!'
 
 
 if __name__ == "__main__":
